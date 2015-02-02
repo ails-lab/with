04bd5d616c259ba253bb3f0723cfcfea4e41844b
Anna Christaki <achristaki@image.ntua.gr>
2015-02-02 15:43:58 +0200

    better masonry loading

--- public/components/main-content/main-content.js
+++ public/components/main-content/main-content.js
@@ -1,28 +1,15 @@
-define(['knockout', 'text!./main-content.html'], function(ko, template) {
+define(['masonry','knockout', 'text!./main-content.html'], function(masonry,ko, template) {
 
   function MainContentModel(params) {
 	  this.route = params.route;
 	  
-	  require([
-		'jquery',
-		'masonry',
-		'imagesloaded',
-		], function($,masonry, imagesloaded) {
-		  
-	
-		  
-		  var msnryboxes = document.querySelectorAll('.js-masonry');
-
-		  [].forEach.call(msnryboxes, function(msnrybox) {
-			  imagesloaded( msnrybox, function() {
-				  new masonry( msnrybox);
-					})	  
-		    
-		  });
-		  
-		
-	     });
+	  var msnryboxes=$('.js-masonry');
 	  
+	  
+	  $('.js-masonry').each(function(index,listitem){
+		  this===listitem;
+		  new masonry( listitem);
+	  });
 	
 	 
   }
