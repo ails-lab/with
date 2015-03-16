98fa0d78a55bae010fbc5d960796ca739fd73deb
fedjo yiorgos <antibra@hotmail.com>
2015-03-16 13:13:35 +0200

    test commit

--- test/test/daoTests/MediaDAOTest.java
+++ test/test/daoTests/MediaDAOTest.java
@@ -23,38 +23,40 @@ import java.net.URISyntaxException;
 import java.net.URL;
 
 import model.Media;
+import model.Record;
 import model.RecordLink;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
 import org.junit.Test;
-import static org.fest.assertions.Assertions.assertThat;
 
+import static org.fest.assertions.Assertions.assertThat;
 import db.DB;
 
 public class MediaDAOTest {
 	@Test
 	public void testMediaStorage() throws IOException, URISyntaxException {
 
+		Media image = null;
 		for(int i = 0; i < 100; i++) {
 			//Create a Media Object
-			Media image = new Media();
+			image = new Media();
 
 
-		URL url = new URL("http://www.ntua.gr/ntua-01.jpg");
-		File file = new File("test_java.txt");
-		FileUtils.copyURLToFile(url, file);
-		FileInputStream fileStream = new FileInputStream(
-				file);
-
-			byte[] rawbytes = IOUtils.toByteArray(fileStream);
-
-
-		image.setData(rawbytes);
-		image.setType("IMAGE");
-		image.setMimeType("image/jpeg");
-		image.setHeight(599);
-		image.setWidth(755);
+			URL url = new URL("http://www.ntua.gr/ntua-01.jpg");
+			File file = new File("test_java.txt");
+			FileUtils.copyURLToFile(url, file);
+			FileInputStream fileStream = new FileInputStream(
+					file);
+	
+				byte[] rawbytes = IOUtils.toByteArray(fileStream);
+	
+	
+			image.setData(rawbytes);
+			image.setType("IMAGE");
+			image.setMimeType("image/jpeg");
+			image.setHeight(599);
+			image.setWidth(755);
 
 			DB.getMediaDAO().makePermanent(image);
 		}
@@ -62,8 +64,8 @@ public class MediaDAOTest {
 		assertThat( image.getDbId()).isNotNull();
 		
 		//Create Record Object
-		//Record record = new Record();
-		//DB.getRecordDAO().save(record);
+		Record record = new Record();
+		DB.getRecordDAO().save(record);
 
 		assertThat( record.getDbID()).isNotNull();
 
