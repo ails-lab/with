fdd7bf15d05d6b4362bab6750a576bdba4403642
Arne Stabenau <stabenau@gmail.com>
2015-04-17 13:52:17 +0300

    wrong code in the class ...

--- test/general/controllerTest/ApiKeyTest.java
+++ test/general/controllerTest/ApiKeyTest.java
@@ -16,6 +16,7 @@
 
 package general.controllerTest;
 
+import static akka.pattern.Patterns.ask;
 import static org.fest.assertions.Assertions.assertThat;
 import static play.mvc.Http.Status.BAD_REQUEST;
 import static play.mvc.Http.Status.OK;
@@ -25,48 +26,83 @@ import static play.test.Helpers.route;
 import static play.test.Helpers.running;
 import static play.test.Helpers.status;
 
+import java.util.Date;
+import java.util.List;
+
+import model.ApiKey;
+import model.ApiKey.CallLimit;
+
 import org.junit.Test;
 
+import play.libs.Akka;
 import play.libs.Json;
 import play.mvc.Result;
+import play.test.Helpers;
+import actors.ApiKeyManager;
+import akka.actor.ActorRef;
+import akka.actor.ActorSelection;
 
 import com.fasterxml.jackson.databind.node.ObjectNode;
+
+import db.DB;
 
 public class ApiKeyTest {
 	public static long HOUR = 3600000;
 
 	@Test
-	public void testWithIp() {
-		
+	public void testAuthorization() {
+
 		final ObjectNode json = Json.newObject();
-		json.put("description", "TEst controller");
-		json.put("title", "The test title");
-		json.put("public", "true");
-		json.put("ownerMail", "heres42@mongo.gr");
+		json.put("searchTerm", "cars");
+		json.put("page", 1 );
+		json.put("pageSize", 20 );
+		json.putArray("source" );
+		
 		running( fakeApplication(), new Runnable() {
 			@Override
 			public void run() {
-				Result result = route(fakeRequest("POST", "/collection/add")
+				// create an ApiKey
+				ApiKey k = new ApiKey();
+				// should cover localhost
+				k.setIpPattern("((0:){7}.*)|(127.0.0.1)");
+				// set it to expired				
+				k.setExpires(new Date(new Date().getTime()-HOUR));
+				k.addCall(0, ".*");
+
+				// store it
+				DB.getApiKeyDAO().makePermanent(k);
+				
+				// read it into the Actor
+				ActorSelection api = Akka.system().actorSelection("user/apiKeyManager"); 
+				api.tell( new ApiKeyManager.Reset(), ActorRef.noSender());
+				
+				Result result = route(fakeRequest("POST", "/api/search")
 						.withJsonBody(json)
 						.withSession("user", "blabla"), HOUR);
-						
-					
+
+			    assertThat(status(result)).isEqualTo(BAD_REQUEST);
+			    assertThat( Helpers.contentAsString(result)).isEqualTo("EXPIRED_IP");
+			    
+			    k.setExpires(new Date(new Date().getTime()+1000*HOUR));
+				DB.getApiKeyDAO().makePermanent(k);
+				api.tell( new ApiKeyManager.Reset(), ActorRef.noSender());
+
+				result = route(fakeRequest("POST", "/api/search")
+						.withJsonBody(json)
+						.withSession("user", "blabla"), HOUR);
 
 			    assertThat(status(result)).isEqualTo(OK);
+			    // assertThat( Helpers.contentAsString(result)).isEqualTo("EXPIRED_IP");
+				
+				ask( api, new ApiKeyManager.Store(), 2000 );
+				ApiKey k2 = DB.getApiKeyDAO().get( k.getDbId());
+				List<CallLimit> limits = k2.getLimits();
+				assertThat( limits.get(0).counter ).isEqualTo(1); 
+				
+				// remove the ApiKeys from DB
+				DB.getApiKeyDAO().delete(k2);
 			}
 		});
 
-	    }
-	@Test
-	public void testNoAuthorization() {
-		running( fakeApplication(), new Runnable() {
-			@Override
-			public void run() {
-				Result result = route(fakeRequest("POST", "/collection/add"), HOUR );
-
-				System.out.println();
-				assertThat(status(result)).isEqualTo(BAD_REQUEST);
-			}
-		});
 	}
 }
