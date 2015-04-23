d7a4f6ef452ae8e10197bb7537cd763d9379e812
gmar gmar <gmar@image.ece.ntua.gr>
2015-04-23 11:46:25 +0300

    ignore setting thumbnail (objectId) field to Collection and CollectionRecord
    Conflicts:
    	app/controllers/AutocompleteController.java

--- app/espace/core/ParallelAPICall.java
+++ app/espace/core/ParallelAPICall.java
@@ -18,29 +18,23 @@ package espace.core;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import play.Logger;
-import play.Logger.ALogger;
-import play.libs.Json;
-
 import java.util.function.BiFunction;
 import java.util.function.Function;
 
-import controllers.UserManager;
 import play.libs.F.Function0;
 import play.libs.F.Promise;
+import play.libs.Json;
 import play.mvc.Result;
-import utils.MethodCallable;
 
 public class ParallelAPICall {
-			
+
 	/**
 	 * @param methodQuery	method that forms and issues the API query
 	 * @param responseCollectionMethod		method that combines the API results and returns them as a List<R>
 	 * @param input		input parameters used as input to methodQuery's call method
 	 * @return 	a list of Promises with <R>
 	 */
-	public static <I, U, R> Promise<R> createPromise(final BiFunction<I, U, R> methodQuery, 
+	public static <I, U, R> Promise<R> createPromise(final BiFunction<I, U, R> methodQuery,
 			final I input1, final U input2) {
 		Promise<R> p = Promise.promise(new Function0<R>() {
 				public R apply() throws Throwable {
@@ -49,15 +43,15 @@ public class ParallelAPICall {
 			 });
 		return p;
 	}
-	
+
 	/**
 	 * @param responseCollectionMethod	method that combines the API results and returns them as a List<R>
 	 * @param promises	list of Promises
 	 * @return 	a Promise with Result
 	 */
 	public static <R> Promise<Result> combineResponses(final Function<R, Boolean> responseCollectionMethod,
-			Iterable<Promise<R>> promises) {		
-		Promise<List<R>> promisesSequence = Promise.sequence(promises);		 
+			Iterable<Promise<R>> promises) {
+		Promise<List<R>> promisesSequence = Promise.sequence(promises);
         Promise<Result> promiseResult = promisesSequence.map(
     		new play.libs.F.Function<Iterable<R>, Result>() {
     			List<R> finalResponses = new ArrayList<R>();
@@ -68,17 +62,17 @@ public class ParallelAPICall {
     			}
     		}
         );
-        return promiseResult;	
+        return promiseResult;
 	}
-	
+
 	public static <R> Promise<Result> combineResponses(final Function<R, Boolean> responseCollectionMethod,
-			Iterable<Promise<R>> promises, final Function<List<R>, List<R>> filter) {	
-		Promise<List<R>> promisesSequence = Promise.sequence(promises);		 
+			Iterable<Promise<R>> promises, final Function<List<R>, List<R>> filter) {
+		Promise<List<R>> promisesSequence = Promise.sequence(promises);
         Promise<Result> promiseResult = promisesSequence.map(
     		new play.libs.F.Function<Iterable<R>, Result>() {
     			public Result apply(Iterable<R> responses) {
     				List<R> combinedResponses, finalResponses = new ArrayList<R>();
-					combinedResponses = iterateResponses(responseCollectionMethod, responses);					
+					combinedResponses = iterateResponses(responseCollectionMethod, responses);
 					finalResponses = filter.apply(combinedResponses);
     				return toStatus(finalResponses);
     				//Logger.debug("Total time for all sources to respond: " + (System.currentTimeMillis()- initTime));
@@ -87,7 +81,7 @@ public class ParallelAPICall {
         );
         return promiseResult;
 	}
-	
+
 	public static <R> List<R> iterateResponses(final java.util.function.Function<R, Boolean> responseCollectionMethod, Iterable<R> responses) {
 		List<R> finalResponses = new ArrayList<R>();
 		for (R r: responses) {
@@ -96,8 +90,8 @@ public class ParallelAPICall {
 		}
 		return finalResponses;
 	}
-	
-	
+
+
 	private static <R> Result toStatus(List<R> responses) {
 		if (!responses.isEmpty()) {
 			return play.mvc.Results.ok(Json.toJson(responses));
@@ -105,5 +99,5 @@ public class ParallelAPICall {
 		else
 			return play.mvc.Results.noContent();
 	}
-	
+
 }
