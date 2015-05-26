2a0a5a3c875036a7d8d75649a7e9da78cd2af1bd
Anna Christaki <achristaki@image.ntua.gr>
2015-05-26 11:22:18 +0300

    cleanup

--- public/components/facets/facets.js
+++ public/components/facets/facets.js
@@ -2,14 +2,9 @@ define(['knockout', 'text!./facets.html'], function(ko, template) {
 
   function FacetsViewModel(params) {
 
-    // This viewmodel doesn't do anything except pass through the 'route' parameter to the view.
-    // You could remove this viewmodel entirely, and define 'nav-bar' as a template-only component.
-    // But in most apps, you'll want some viewmodel logic to determine what navigation options appear.
-
+   
     this.route = params.route;
-    if(params.length==0){
-    	
-    }
+   
     var self=this;
     self.visiblePanel=ko.observable(false);
     
@@ -20,10 +15,7 @@ define(['knockout', 'text!./facets.html'], function(ko, template) {
     		self.visiblePanel(false);
     }
     
-    self.changeSearch=function(){
-    	//ko.contextFor(withsearchid).$data.search();
-    }
-    
+   
     
   }
 
