2d6f2a6c520103e280cc952508e3b4e5eb1d8a6f
Marios Phinikettos <FinikM@gmail.com>
2015-06-10 17:44:03 +0300

    Displaying a thumbnail of the uploaded image

--- public/components/media-uploader/media-uploader.js
+++ public/components/media-uploader/media-uploader.js
@@ -12,6 +12,18 @@ define(['knockout', 'text!./image-upload.html', 'app', 'knockout-validation', 'j
 		self.description = ko.observable().extend();
 		self.imageURL    = ko.observable();
 
+		$('#imageupload').fileupload({
+			add : function(e, data) {
+				if (data.files && data.files[0]) {
+					var reader    = new FileReader();
+					reader.onload = function(e) {
+						self.resizePhoto(e.target.result, 200);
+					};
+					reader.readAsDataURL(data.files[0]);
+				}
+			}
+		});
+
 		self.close       = function() {
 			app.closePopup();
 		};
@@ -19,6 +31,37 @@ define(['knockout', 'text!./image-upload.html', 'app', 'knockout-validation', 'j
 		self.uploadImage = function() {
 			console.log("Uploading Image");
 		};
+
+		self.resizePhoto      = function(src, width, height) {
+			var img    = new Image();
+			img.onload = function() {
+				var canvas     = document.createElement('canvas');
+				var ctx        = canvas.getContext("2d");
+				ctx.drawImage(img, 0, 0);
+
+				height = typeof height !== 'undefined' ? height : -1;
+
+				if (height < 0) {
+					if (img.width > img.height) {
+						height = (img.height / img.width) * width;
+					}
+					else {
+						width = (img.width / img.height) * height;
+					}
+				}
+
+				canvas.width  = width;
+				canvas.height = height;
+				ctx           = canvas.getContext("2d");
+				ctx.drawImage(img, 0, 0, width, height);
+
+				var dataurl   = canvas.toDataURL("image/png");
+
+				self.imageURL(dataurl);
+			};
+			img.src = src;
+		};
+
 	}
 
 	return { viewModel: MediaUploaderModel, template: template };
