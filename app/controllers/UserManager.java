46867b4c12c1366cb9a81a7eeaf794568d2bb290
michael <michael@kithira>
2015-06-12 15:21:14 +0300

    fix

--- app/controllers/UserManager.java
+++ app/controllers/UserManager.java
@@ -721,7 +721,9 @@ public class UserManager extends Controller {
 		
 		String enc = encryptToken(u.getDbId().toString());
 		
-		String resetURL = APPLICATION_URL;
+		//String resetURL = APPLICATION_URL;
+		String resetURL = "http://localhost:9000/assets/index.html#reset";
+		
 		//This will retrieve line separator dependent on OS.
 		String newLine = System.getProperty("line.separator");
 		
@@ -765,11 +767,8 @@ public class UserManager extends Controller {
 		
 	}
 	
-	/***
-	 * 	
-	 * 	Requests user's new password, checks for length.
-	 * 
-	 *  Parses token from the URL sent in the resetPassword() email.
+	/*** 	
+	 * 	Parses token from the URL sent in the resetPassword() email.
 	 *  
 	 *  Changes stored password.
 	 * 
@@ -778,7 +777,6 @@ public class UserManager extends Controller {
 	
 	public static Result changePassword(){
 		
-		//123123
 		
 		ObjectNode result = Json.newObject();
 		
@@ -834,8 +832,7 @@ public class UserManager extends Controller {
 						u.setPassword(newPassword);
 						DB.getUserDAO().makePermanent(u);
 						result = (ObjectNode) Json.parse(DB.getJson(u));
-						//testing
-						//result.remove("md5Password");
+						result.remove("md5Password");
 						return ok(result);
 					}else{
 						result.put("error", "User does not exist");
@@ -851,7 +848,6 @@ public class UserManager extends Controller {
 		}
 		
 		return badRequest(result);
-		//token = "e27c561866fc57e033496f06981f8dcd30eea5e9f01a0968a70a2b6f7e7b39bd8930a828079cb37035265c36d30b449ed03763998ba95ae3c414332dfb0343d8c8c91f9fb165033aed4c1c44cc559d793387b04d49072200e31785548192fe51";
 
 		
 	}
