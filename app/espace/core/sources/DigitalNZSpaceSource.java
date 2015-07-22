aba4d5f84fddce9e79117f6bce1bbb10e40731fb
Kostas Sismanis <ksismanis@image.ece.ntua.gr>
2015-07-22 15:23:28 +0300

    rights filter expression

--- app/espace/core/sources/DigitalNZSpaceSource.java
+++ app/espace/core/sources/DigitalNZSpaceSource.java
@@ -68,14 +68,14 @@ public class DigitalNZSpaceSource extends ISpaceSource {
 		addMapping(CommonFilters.TYPE_ID, TypeValues.TEXT, "Books");
 		
 		
-	/*	addMapping(CommonFilters.RIGHTS_ID, RightsValues.Creative_Commercial, ".*(creative)(?!.*nc).*(nd).*");
+		addMapping(CommonFilters.RIGHTS_ID, RightsValues.Creative_Commercial, ".*(creative)(?!.*nc).*(nd).*");
 		addMapping(CommonFilters.RIGHTS_ID, RightsValues.Creative_Modify, ".*(creative)(?!.*nd).*(nc).*");
 		addMapping(CommonFilters.RIGHTS_ID, RightsValues.Creative_Commercial_and_Modify, ".*(creative)(?!.*nc)(?!.*nd).*");
-		addMapping(CommonFilters.RIGHTS_ID, RightsValues.Creative, ".*(creative).*");*/
+		addMapping(CommonFilters.RIGHTS_ID, RightsValues.Creative, ".*(creative).*");
 		
-		addMapping(CommonFilters.RIGHTS_ID, RightsValues.Public, "http://creativecommons.org/publicdomain/mark/1.0/","http://creativecommons.org/publicdomain/zero/1.0/","http://creativecommons.org/licenses/by/","http://creativecommons.org/licenses/by-sa/");
+		/*addMapping(CommonFilters.RIGHTS_ID, RightsValues.Public, "http://creativecommons.org/publicdomain/mark/1.0/","http://creativecommons.org/publicdomain/zero/1.0/","http://creativecommons.org/licenses/by/","http://creativecommons.org/licenses/by-sa/");
 		addMapping(CommonFilters.RIGHTS_ID, RightsValues.Restricted, "http://creativecommons.org/licenses/by-nc/","http://creativecommons.org/licenses/by-nc-sa/","http://creativecommons.org/licenses/by-nc-nd/","http://creativecommons.org/licenses/by-nd/","http://www.europeana.eu/rights/out-of-copyright-non-commercial/");
-		addMapping(CommonFilters.RIGHTS_ID, RightsValues.Permission, "http://creativecommons.org/licenses/by-nc/","http://creativecommons.org/licenses/by-nc-sa/","http://creativecommons.org/licenses/by-nc-nd/","http://creativecommons.org/licenses/by-nd/","http://creativecommons.org/publicdomain/mark/1.0/","http://creativecommons.org/publicdomain/zero/1.0/","http://creativecommons.org/licenses/by/","http://creativecommons.org/licenses/by-sa/","http://www.europeana.eu/rights/out-of-copyright-non-commercial/");
+		addMapping(CommonFilters.RIGHTS_ID, RightsValues.Permission, "http://creativecommons.org/licenses/by-nc/","http://creativecommons.org/licenses/by-nc-sa/","http://creativecommons.org/licenses/by-nc-nd/","http://creativecommons.org/licenses/by-nd/","http://creativecommons.org/publicdomain/mark/1.0/","http://creativecommons.org/publicdomain/zero/1.0/","http://creativecommons.org/licenses/by/","http://creativecommons.org/licenses/by-sa/","http://www.europeana.eu/rights/out-of-copyright-non-commercial/");*/
 		
 	}
 	
