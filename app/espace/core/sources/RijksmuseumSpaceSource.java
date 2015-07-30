a67df650afcafaea3b886b35ae070b5e15bcea7d
Enrique Matos Alfonso <gardero@gmail.com>
2015-07-30 14:22:06 +0300

    fix in rm

--- app/espace/core/sources/RijksmuseumSpaceSource.java
+++ app/espace/core/sources/RijksmuseumSpaceSource.java
@@ -34,15 +34,15 @@ import espace.core.SourceResponse.ItemsResponse;
 import espace.core.SourceResponse.MyURL;
 import play.libs.Json;
 
-public class RijksmuseumSpaceSource extends ISpaceSource{
+public class RijksmuseumSpaceSource extends ISpaceSource {
 	public static final String LABEL = "Rijksmuseum";
-	private String apikey="SECRET_KEY";
+	private String apikey = "SECRET_KEY";
 
 	@Override
 	public String getSourceName() {
 		return LABEL;
 	}
-	
+
 	public String getHttpQuery(CommonQuery q) {
 		QueryBuilder builder = new QueryBuilder("https://www.rijksmuseum.nl/api/en/collection");
 		builder.addSearchParam("key", apikey);
@@ -51,7 +51,7 @@ public class RijksmuseumSpaceSource extends ISpaceSource{
 		builder.addSearchParam("p", "" + ((Integer.parseInt(q.page) - 1) * Integer.parseInt(q.pageSize) + 1));
 
 		builder.addSearchParam("ps", "" + q.pageSize);
-		
+
 		return addfilters(q, builder).getHttp();
 	}
 
@@ -62,108 +62,113 @@ public class RijksmuseumSpaceSource extends ISpaceSource{
 		String httpQuery = getHttpQuery(q);
 		res.query = httpQuery;
 		JsonNode response;
-//		CommonFilterLogic type = CommonFilterLogic.typeFilter();
-//		CommonFilterLogic provider = CommonFilterLogic.providerFilter();
-//		CommonFilterLogic dataprovider = CommonFilterLogic.dataproviderFilter();
-//		CommonFilterLogic creator = CommonFilterLogic.creatorFilter();
-//		CommonFilterLogic rights = CommonFilterLogic.rightsFilter();
-//		CommonFilterLogic country = CommonFilterLogic.countryFilter();
-//		CommonFilterLogic year = CommonFilterLogic.yearFilter();
-//		CommonFilterLogic contributor = CommonFilterLogic.contributorFilter();
-		if (checkFilters(q)){
-		try {
-			response = HttpConnector.getURLContent(httpQuery);
-			res.totalCount = Utils.readIntAttr(response, "count", true);
-//			res.count = q.pageSize;
-			ArrayList<ItemsResponse> a = new ArrayList<ItemsResponse>();
+		// CommonFilterLogic type = CommonFilterLogic.typeFilter();
+		// CommonFilterLogic provider = CommonFilterLogic.providerFilter();
+		// CommonFilterLogic dataprovider =
+		// CommonFilterLogic.dataproviderFilter();
+		// CommonFilterLogic creator = CommonFilterLogic.creatorFilter();
+		// CommonFilterLogic rights = CommonFilterLogic.rightsFilter();
+		// CommonFilterLogic country = CommonFilterLogic.countryFilter();
+		// CommonFilterLogic year = CommonFilterLogic.yearFilter();
+		// CommonFilterLogic contributor =
+		// CommonFilterLogic.contributorFilter();
+		if (checkFilters(q)) {
+			try {
+				response = HttpConnector.getURLContent(httpQuery);
+				res.totalCount = Utils.readIntAttr(response, "count", true);
+				// res.count = q.pageSize;
+				ArrayList<ItemsResponse> a = new ArrayList<ItemsResponse>();
 				for (JsonNode item : response.path("artObjects")) {
 					ItemsResponse it = new ItemsResponse();
 					// countValue(type, t);
-					it.id = Utils.readAttr(item, "id", true);
+					it.id = Utils.readAttr(item, "objectNumber", true);
 					it.title = Utils.readLangAttr(item, "title", false);
 					it.creator = Utils.readLangAttr(item, "principalOrFirstMaker", false);
-					
+
 					it.thumb = Utils.readArrayAttr(item.path("webImage"), "url", false);
-					
+
 					it.fullresolution = Utils.readArrayAttr(item.path("headerImage"), "url", false);
-//					it.description = Utils.readLangAttr(item, "dcDescription", false);
-//					it.year = Utils.readArrayAttr(item, "year", false);
-//					it.dataProvider = Utils.readLangAttr(item, "edmDataProvider",
-//							false);
+					// it.description = Utils.readLangAttr(item,
+					// "dcDescription", false);
+					// it.year = Utils.readArrayAttr(item, "year", false);
+					// it.dataProvider = Utils.readLangAttr(item,
+					// "edmDataProvider",
+					// false);
 					it.url = new MyURL();
 					it.url.original = it.fullresolution;
-					it.url.fromSourceAPI = "https://www.rijksmuseum.nl/en/search/objecten?ii="+it.id;
-//					it.rights = Utils.readLangAttr(item, "rights", false);
-//					it.externalId = it.fullresolution.get(0);
-//					if (it.externalId == null || it.externalId == "")
-//						it.externalId = it.url.original.get(0);
-//					it.externalId = DigestUtils.md5Hex(it.externalId);
+					it.url.fromSourceAPI = "https://www.rijksmuseum.nl/en/search/objecten?q=dance&p=1&ps=12&ii=0#/"
+							+ it.id + ",0";
+					// it.rights = Utils.readLangAttr(item, "rights", false);
+					// it.externalId = it.fullresolution.get(0);
+					// if (it.externalId == null || it.externalId == "")
+					// it.externalId = it.url.original.get(0);
+					// it.externalId = DigestUtils.md5Hex(it.externalId);
 					a.add(it);
 				}
-			res.items = a;
-			res.count = a.size();
-			
-			res.facets = response.path("facets");
+				res.items = a;
+				res.count = a.size();
 
-//			for (JsonNode facet : response.path("facets")) {
-//				for (JsonNode jsonNode : facet.path("fields")) {
-//					String label = jsonNode.path("label").asText();
-//					int count = jsonNode.path("count").asInt();
-//					switch (facet.path("name").asText()) {
-//					case "TYPE":
-//						countValue(type, label, count);
-//						break;
-//
-//					case "DATA_PROVIDER":
-//						countValue(dataprovider, label, false, count);
-//						break;
-//						
-//					case "PROVIDER":
-//						countValue(provider, label, false, count);
-//						break;
-//
-//					case "RIGHTS":
-//						countValue(rights, label, count);
-//						break;
-//
-//					case "proxy_dc_creator":
-//						countValue(creator, label, false, count);
-//						break;
-////					case "proxy_dc_contributor":
-////						countValue(contributor, label, false, count);
-////						break;
-//					case "COUNTRY":
-//						countValue(country, label, false, count);
-//						break;
-//
-//					case "YEAR":
-//						countValue(year, label, false, count);
-//						break;
-//
-//					default:
-//						break;
-//					}
-//				}
-//			}
+				res.facets = response.path("facets");
 
-			res.filtersLogic = new ArrayList<>();
-//			res.filtersLogic.add(type);
-//			res.filtersLogic.add(provider);
-//			res.filtersLogic.add(dataprovider);
-//			res.filtersLogic.add(creator);
-////			res.filtersLogic.add(contributor);
-//			res.filtersLogic.add(rights);
-//			res.filtersLogic.add(country);
-//			res.filtersLogic.add(year);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}}
+				// for (JsonNode facet : response.path("facets")) {
+				// for (JsonNode jsonNode : facet.path("fields")) {
+				// String label = jsonNode.path("label").asText();
+				// int count = jsonNode.path("count").asInt();
+				// switch (facet.path("name").asText()) {
+				// case "TYPE":
+				// countValue(type, label, count);
+				// break;
+				//
+				// case "DATA_PROVIDER":
+				// countValue(dataprovider, label, false, count);
+				// break;
+				//
+				// case "PROVIDER":
+				// countValue(provider, label, false, count);
+				// break;
+				//
+				// case "RIGHTS":
+				// countValue(rights, label, count);
+				// break;
+				//
+				// case "proxy_dc_creator":
+				// countValue(creator, label, false, count);
+				// break;
+				//// case "proxy_dc_contributor":
+				//// countValue(contributor, label, false, count);
+				//// break;
+				// case "COUNTRY":
+				// countValue(country, label, false, count);
+				// break;
+				//
+				// case "YEAR":
+				// countValue(year, label, false, count);
+				// break;
+				//
+				// default:
+				// break;
+				// }
+				// }
+				// }
+
+				res.filtersLogic = new ArrayList<>();
+				// res.filtersLogic.add(type);
+				// res.filtersLogic.add(provider);
+				// res.filtersLogic.add(dataprovider);
+				// res.filtersLogic.add(creator);
+				//// res.filtersLogic.add(contributor);
+				// res.filtersLogic.add(rights);
+				// res.filtersLogic.add(country);
+				// res.filtersLogic.add(year);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 		// protected void countValue(CommonFilterResponse type, String t) {
 		// type.addValue(vmap.translateToCommon(type.filterID, t));
 		// }
 		return res;
 	}
 
-	
 }
