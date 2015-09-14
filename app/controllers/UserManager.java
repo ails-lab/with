d803ffac099e3852a22b64da66a28543633b6f9e
michael <michael@kithira>
2015-09-14 11:46:14 +0300

    share collection autocomplete for groups

--- app/controllers/UserManager.java
+++ app/controllers/UserManager.java
@@ -20,6 +20,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Random;
@@ -474,10 +475,44 @@ public class UserManager extends Controller {
 	 */
 	public static Result listNames(String prefix) {
 		List<User> users = DB.getUserDAO().getByUsernamePrefix(prefix);
-		ArrayNode result = Json.newObject().arrayNode();
+
+		ArrayNode suggestions = Json.newObject().arrayNode();
 		for (User user : users) {
-			result.add(user.getUsername());
+			ObjectNode node = Json.newObject();
+			ObjectNode data = Json.newObject().objectNode();
+			data.put("type", "user");
+			//costly?
+			node.put("value", user.getUsername());
+			node.put("data", data);
+			suggestions.add(node);
+			//result.add(user.getUsername());
 		}
+		
+		//List<UserGroup> groups = DB.getUserGroupDAO().getByUsernamePrefix(prefix);
+		List<String> groups = new ArrayList<String>();  //remember to remove import, change stuff to usergroup from string!!
+		groups.add("atest1");
+		groups.add("atest2");
+		groups.add("btest3");
+		groups.add("etest4");
+		for (String group : groups) {
+			ObjectNode node = Json.newObject().objectNode();
+			ObjectNode data = Json.newObject().objectNode();
+			data.put("type", "group");
+			//username.put("name", user.getUsername());
+			node.put("value", group);
+			node.put("data", data);
+			suggestions.add(node);
+			//result.add(user.getUsername());
+		}
+		
+		ArrayNode result = Json.newObject().arrayNode();
+		
+		ObjectNode x = Json.newObject().objectNode();
+		
+		x.put("suggestions", suggestions);
+		
+		result.add(x);
+		
 		return ok(result);
 	}
 
