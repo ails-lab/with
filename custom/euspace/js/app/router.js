235eb9ad4b3178480d7702c0101a888082529329
Anna Christaki <achristaki@image.ntua.gr>
2015-09-18 15:07:53 +0300

    custom design for euspace

---
+++ custom/euspace/js/app/router.js
@@ -0,0 +1,53 @@
+define(["knockout", "crossroads", "hasher"], function(ko, crossroads, hasher) {
+
+	// This module configures crossroads.js, a routing library. If you prefer, you
+	// can use any other routing library (or none at all) as Knockout is designed to
+	// compose cleanly with external libraries.
+	//
+	// You *don't* have to follow the pattern established here (each route entry
+	// specifies a 'page', which is a Knockout component) - there's nothing built into
+	// Knockout that requires or even knows about this technique. It's just one of
+	// many possible ways of setting up client-side routes.
+
+	return new Router({
+		routes: [
+			{ url: '',          params: { page: 'home-page',     title: 'Home' } },
+			{ url: 'search',    params: { page: 'search-page',   title: 'Search' } },
+			{ url: 'login',     params: { page: 'login-page',    title: 'Login' } },
+			{ url: 'profile',     params: { page: 'profile',    title: 'Profile' } },
+			{ url: 'mycollections',     params: { page: 'mycollections',    title: 'My Collections',    showsExhibitions: false } },
+			{ url: 'myexhibitions',     params: { page: 'mycollections',    title: 'My Exhibitions',   showsExhibitions: true } },
+			{ url: 'register',  params: { page: 'register-page', title: 'Register'} },
+			{ url: 'email',     params: { page: 'email-page',    title: 'Register' } },
+			{ url: 'collect/{id}',     params: { page: 'item-view',    title: 'Collect' } },
+			{ url: 'collectionview/{id}',     params: { page: 'collection-view',    title: 'Collection View' } },
+			{ url: 'reset/{token}', params: { page: 'new-password', title: 'Reset Password' } },
+			{ url: 'exhibition-edit',     params: { page: 'exhibition-edit',    title: 'Exhibition Edit' } },
+			{ url: 'exhibition-edit/{id}',     params: { page: 'exhibition-edit',    title: 'Exhibition Edit' } },
+			{ url: 'apidoc', params: { page: 'api-docu', title: 'API Documentation' } },
+			{ url: 'testsearch', params: { page: 'testsearch', title: 'testsearch' } },
+			{ url: 'myfavorites', params: { page: 'myfavorites', title: 'My Favorites' } },
+			{ url: 'gallery/{id}/{skin}',     params: { page: '3DRoom',    title: 'Gallery View' } }
+		]
+	});
+
+	function Router(config) {
+		var currentRoute = this.currentRoute = ko.observable({});
+
+		ko.utils.arrayForEach(config.routes, function(route) {
+			crossroads.addRoute(route.url, function(requestParams) {
+				currentRoute(ko.utils.extend(requestParams, route.params));
+			});
+		});
+
+		activateCrossroads();
+	}
+
+	function activateCrossroads() {
+		function parseHash(newHash, oldHash) { crossroads.parse(newHash); }
+		crossroads.normalizeFn = crossroads.NORM_AS_OBJECT;
+		hasher.initialized.add(parseHash);
+		hasher.changed.add(parseHash);
+		hasher.init();
+	}
+});
