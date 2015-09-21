36eb8aa7d9d5ba6461e233c9a61e1404770635b5
Enrique Matos Alfonso <gardero@gmail.com>
2015-09-21 11:55:31 +0300

    added type filter

--- app/espace/core/sources/BritishLibrarySpaceSource.java
+++ app/espace/core/sources/BritishLibrarySpaceSource.java
@@ -18,12 +18,15 @@ package espace.core.sources;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
 
 import org.apache.commons.codec.digest.DigestUtils;
 
 import com.fasterxml.jackson.databind.JsonNode;
 
 import espace.core.CommonFilterLogic;
+import espace.core.CommonFilters;
 import espace.core.CommonQuery;
 import espace.core.FacetsModes;
 import espace.core.HttpConnector;
@@ -33,7 +36,9 @@ import espace.core.SourceResponse;
 import espace.core.Utils;
 import espace.core.SourceResponse.ItemsResponse;
 import espace.core.SourceResponse.MyURL;
+import espace.core.Utils.Pair;
 import play.libs.Json;
+import utils.ListUtils;
 
 public class BritishLibrarySpaceSource extends ISpaceSource {
 	public static final String LABEL = "The British Library";
@@ -44,15 +49,36 @@ public class BritishLibrarySpaceSource extends ISpaceSource {
 		return LABEL;
 	}
 
+	public BritishLibrarySpaceSource() {
+		super();
+		addDefaultWriter(CommonFilters.TYPE_ID, fwriter("media"));
+		// addDefaultWriter(CommonFilters.COUNTRY_ID,
+		// fwriter("sourceResource.spatial.country"));
+
+		addMapping(CommonFilters.TYPE_ID, TypeValues.IMAGE, "photos");
+		addMapping(CommonFilters.TYPE_ID, TypeValues.VIDEO, "videos");
+	}
+
+	private Function<List<String>, Pair<String>> fwriter(String parameter) {
+		Function<String, String> function = (String s) -> {
+			return "%22" + Utils.spacesFormatQuery(s, "%20") + "%22";
+		};
+		return new Function<List<String>, Pair<String>>() {
+			@Override
+			public Pair<String> apply(List<String> t) {
+				return new Pair<String>(parameter, Utils.getORList(ListUtils.transform(t, function), false));
+			}
+		};
+	}
+
 	public String getHttpQuery(CommonQuery q) {
-		QueryBuilder builder = new QueryBuilder(
-				""
-				+ "https://api.flickr.com/services/rest/");
-		builder.addSearchParam("method","flickr.photos.search");
+		QueryBuilder builder = new QueryBuilder("" + "https://api.flickr.com/services/rest/");
+		builder.addSearchParam("method", "flickr.photos.search");
 		builder.addSearchParam("api_key", apikey);
 		builder.addSearchParam("format", "json");
-		builder.addSearchParam("user_id","12403504%40N02");
-		builder.addSearchParam("extras", "description,%20license,%20date_upload,%20date_taken,%20owner_name,%20icon_server,%20original_format,%20last_update,%20geo,%20tags,%20machine_tags,%20o_dims,%20views,%20media,%20path_alias,%20url_sq,%20url_t,%20url_s,%20url_q,%20url_m,%20url_n,%20url_z,%20url_c,%20url_l,%20url_o");
+		builder.addSearchParam("user_id", "12403504%40N02");
+		builder.addSearchParam("extras",
+				"description,%20license,%20date_upload,%20date_taken,%20owner_name,%20icon_server,%20original_format,%20last_update,%20geo,%20tags,%20machine_tags,%20o_dims,%20views,%20media,%20path_alias,%20url_sq,%20url_t,%20url_s,%20url_q,%20url_m,%20url_n,%20url_z,%20url_c,%20url_l,%20url_o");
 		builder.addQuery("text", q.searchTerm);
 		builder.addSearchParam("page", "" + ((Integer.parseInt(q.page) - 1) * Integer.parseInt(q.pageSize) + 1));
 
@@ -72,8 +98,8 @@ public class BritishLibrarySpaceSource extends ISpaceSource {
 		if (checkFilters(q)) {
 			try {
 				String urlStringContent = HttpConnector.getURLStringContent(httpQuery);
-				int s = urlStringContent.indexOf("({")+1;
-				int e = urlStringContent.lastIndexOf("})")+1;
+				int s = urlStringContent.indexOf("({") + 1;
+				int e = urlStringContent.lastIndexOf("})") + 1;
 				String substring = urlStringContent.substring(s, e);
 				response = Json.parse(substring);
 				res.totalCount = Utils.readIntAttr(response.path("photos"), "total", true);
@@ -84,18 +110,18 @@ public class BritishLibrarySpaceSource extends ISpaceSource {
 					// countValue(type, t);
 					it.id = Utils.readAttr(item, "id", true);
 					it.title = Utils.readAttr(item, "title", false);
-//					it.creator = Utils.readAttr(item, "principalOrFirstMaker", false);
+					// it.creator = Utils.readAttr(item,
+					// "principalOrFirstMaker", false);
 
 					it.thumb = Utils.readArrayAttr(item, "url_s", false);
 					it.fullresolution = Utils.readArrayAttr(item, "url_o", false);
-					it.description = Utils.readAttr(item.path("description"),"_content", false);
+					it.description = Utils.readAttr(item.path("description"), "_content", false);
 					it.year = Arrays.asList(Utils.readAttr(item, "datetaken", false).substring(0, 4));
 					System.out.println(it.year);
 					it.dataProvider = LABEL;
 					it.url = new MyURL();
 					it.url.original = it.fullresolution;
-					it.url.fromSourceAPI = "https://www.flickr.com/photos/britishlibrary/"
-							+ it.id + "/";
+					it.url.fromSourceAPI = "https://www.flickr.com/photos/britishlibrary/" + it.id + "/";
 					// it.rights = Utils.readLangAttr(item, "rights", false);
 					it.externalId = it.fullresolution.get(0);
 					it.externalId = DigestUtils.md5Hex(it.externalId);
@@ -105,7 +131,6 @@ public class BritishLibrarySpaceSource extends ISpaceSource {
 				res.count = a.size();
 
 				res.facets = response.path("facets");
-
 
 				res.filtersLogic = new ArrayList<>();
 			} catch (Exception e) {
