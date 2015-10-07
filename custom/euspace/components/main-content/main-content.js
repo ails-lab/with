07014d9f8b7bfd5804b503a1d236663835a52107
eirini <kaldeli@gmail.com>
2015-10-07 15:57:59 +0300

    Updated call to fetch project's collection.

--- custom/euspace/components/main-content/main-content.js
+++ custom/euspace/components/main-content/main-content.js
@@ -214,7 +214,7 @@ define(['bridget','knockout', 'text!./main-content.html','isotope','imagesloaded
 				dataType: "json",
 				url: "/collection/list",
 				processData: false,
-				data: "loggedInUserAccess=read&offset=0&count=20&collectionHits=true&directlyAccessedByGroupName="+JSON.stringify([{group:"EUScreenXL",rights:"OWN"}]),
+				data: "offset=0&count=20&collectionHits=true&directlyAccessedByGroupName="+JSON.stringify([{group:"EUscreenXL",rights:"READ"}]),
 			}).success (function(){
 			});
 		};
@@ -243,7 +243,7 @@ define(['bridget','knockout', 'text!./main-content.html','isotope','imagesloaded
 				self.loading(true);
 				var offset = self.homecollections().length+1;
 				$.ajax({
-					"url": "/collection/list?loggedInUserAccess=read&count=20&offset=" + offset + "&directlyAccessedByGroupName="+JSON.stringify([{group:"EUScreenXL",rights:"OWN"}])
+					"url": "/collection/list?count=20&offset=" + offset + "&directlyAccessedByGroupName="+JSON.stringify([{group:"EUscreenXL",rights:"READ"}]),
 					"method": "get",
 					"contentType": "application/json",
 					"success": function (data) {
