7182459b7002d80f8921db3d6a632e0906c9420b
Anna Christaki <achristaki@image.ntua.gr>
2015-10-19 11:46:53 +0300

    bug fixes

--- custom/espace/components/top-bar/top-bar.js
+++ custom/espace/components/top-bar/top-bar.js
@@ -1,8 +1,15 @@
 define(['knockout', 'text!./top-bar.html', 'app', 'autocomplete'], function(ko, template, app, autocomplete) {
 
+	
+	
+	
   function TopBarViewModel(params) {
 	  this.route = params.route;
 	  
+	  
+	 
+	
+	
 	}
 
 	return { viewModel: TopBarViewModel, template: template };
--- custom/espace/js/app/app.js
+++ custom/espace/js/app/app.js
@@ -23,7 +23,54 @@ define("app", ['knockout'], function (ko) {
 	if (isFirefox) {
 		self.transDuration = 0;
 	}
-
+   
+	
+	self.isLogged=function(){
+		var user=null;
+		var usercookie=readCookie("PLAY_SESSION")
+		if(usercookie)
+		usercookie.replace(/\"/g, "");
+		if(usercookie!=null){
+		   var keys=ExtractQueryString(usercookie);	
+		  
+         
+		    return (keys["user"]==undefined ? false : true);
+		}else{return false;}
+		
+	};
+	
+	self.gotoWith=function(){
+		
+		    window.childwith=window.open('http://with.image.ntua.gr/assets/index.html#mycollections', 'with');
+		  
+		    window.childwith.focus();
+		   		
+	}
+	
+	function readCookie(name) {
+	    var nameEQ = encodeURIComponent(name) + "=";
+	    var ca = document.cookie.split(';');
+	    for (var i = 0; i < ca.length; i++) {
+	        var c = ca[i];
+	        while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+	        if (c.indexOf(nameEQ) === 0) return decodeURIComponent(c.substring(nameEQ.length, c.length));
+	    }
+	    return null;
+	}
+	  
+	function ExtractQueryString(cookie) {
+	    var oResult = {};
+	    var aQueryString = cookie.split("&");
+	    for (var i = 0; i < aQueryString.length; i++) {
+	        var aTemp = aQueryString[i].split("=");
+	        if (aTemp[1].length > 0) {
+	            oResult[aTemp[0]] = unescape(aTemp[1]);
+	        }
+	    }
+	    return oResult;
+	} 
+	
+	
 	 require(["./js/plugin"], function(EUSpaceApp) {
 		 window.EUSpaceUI=new EUSpaceApp.EUSpaceApp.ui({
 
