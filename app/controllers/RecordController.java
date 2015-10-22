92386ecbf2c3bc0911b6525527ae327034b19629
eirini <kaldeli@gmail.com>
2015-10-22 16:16:37 +0300

    Error fixes.

--- app/controllers/RecordController.java
+++ app/controllers/RecordController.java
@@ -236,7 +236,7 @@ public class RecordController extends Controller {
 		ArrayNode result = Json.newObject().arrayNode();
 		CollectionRecord r = DB.getCollectionRecordDAO().getByExternalId(externalId).get(0);
 		String title = r.getTitle();
-		String provider = r.getProvenanceChain().get(0).provider;
+		String provider = r.getProvenanceChain().get(0).providerName;
 
 		/*
 		 * Search for available collections
