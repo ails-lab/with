e6b3ae6d9587650e6129c3ffa0758df2334b4889
eirini <kaldeli@gmail.com>
2016-01-13 12:51:04 +0200

    Adds some extra methods in WithAccess.

--- app/model/basicDataTypes/WithAccess.java
+++ app/model/basicDataTypes/WithAccess.java
@@ -99,6 +99,21 @@ public class WithAccess extends HashMap<ObjectId, Access> {
 	public void setAcl(List<AccessEntry> acl) {
 		this.acl = acl;
 	}
+	
+	public Access getAcl(ObjectId user) {
+		for (AccessEntry ae: acl) {
+			if (ae.user.equals(user))
+				return ae.level;
+		}
+		return Access.NONE;
+	}
 
+	/*public List<User> getUsersWithAccess(Access access) {
+		for (AccessEntry ae: acl) {
+			if (ae.level)
+				return ae.level;
+		}
+		return Access.NONE;
+	}*/
 
 }
