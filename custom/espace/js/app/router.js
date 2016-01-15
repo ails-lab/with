d8ce469039bfc4d80a59907807f79e0cb64fd110
Anna Christaki <achristaki@image.ntua.gr>
2016-01-15 12:12:15 +0200

    for ipa testing

--- custom/espace/js/app/router.js
+++ custom/espace/js/app/router.js
@@ -69,7 +69,7 @@ define(["knockout", "crossroads", "hasher"], function(ko, crossroads, hasher) {
 					// toggle button
 					$( '.mobilemenu' ).toggleClass( 'active' );
 	          }
-	          setTimeout(function(){ WITHApp.init(); }, 600);
+	          setTimeout(function(){ console.log("with app init");WITHApp.init(); }, 600);
 	          
 	         
 	     	 
