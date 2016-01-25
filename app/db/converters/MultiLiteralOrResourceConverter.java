be83b15a2eba8e53a144f8299f3d746891869ff7
eirini <kaldeli@gmail.com>
2016-01-25 10:55:52 +0200

    Adds new converter.

---
+++ app/db/converters/MultiLiteralOrResourceConverter.java
@@ -0,0 +1,59 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+
+package db.converters;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import model.basicDataTypes.MultiLiteralOrResource;
+
+import org.mongodb.morphia.annotations.Converters;
+import org.mongodb.morphia.converters.TypeConverter;
+import org.mongodb.morphia.mapping.MappedField;
+
+import com.mongodb.BasicDBList;
+
+@Converters(MultiLiteralOrResourceConverter.class)
+public class MultiLiteralOrResourceConverter extends TypeConverter{
+
+	public MultiLiteralOrResourceConverter() {
+		super(MultiLiteralOrResource.class);
+	}
+
+	@Override
+	public Object decode(Class targetClass, Object fromDBObject,
+			MappedField optionalExtraInfo) {
+		if (fromDBObject == null) 
+            return null;
+		else {
+			MultiLiteralOrResource outMap = new MultiLiteralOrResource();
+			for (Entry<String, BasicDBList> e: ((Map<String, BasicDBList>) fromDBObject).entrySet()) {
+				ArrayList<String> outList = new ArrayList<String>();
+				for (Object s: e.getValue()) {
+					outList.add((String) s);
+				}
+				outMap.put(e.getKey(), outList);
+			}
+			return outMap;
+		}
+	}
+
+}
