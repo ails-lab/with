f0540703f20fb05b5530225d0b9c8dd6d5a3b448
fedjo yiorgos <antibra@hotmail.com>
2016-01-26 10:07:58 +0200

    remove imports, fix compilation errors

--- app/sources/formatreaders/DPLARecordFormatter.java
+++ app/sources/formatreaders/DPLARecordFormatter.java
@@ -18,8 +18,6 @@ package sources.formatreaders;
 
 import java.util.List;
 
-import com.sun.javafx.animation.TickCalculation;
-
 import model.EmbeddedMediaObject;
 import model.EmbeddedMediaObject.MediaVersion;
 import model.EmbeddedMediaObject.WithMediaRights;
