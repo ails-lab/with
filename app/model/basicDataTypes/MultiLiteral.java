213010b6677bdaca043efc5d9bc7d4f2c95cec3f
Enrique Matos Alfonso <gardero@gmail.com>
2016-01-27 11:56:31 +0200

    fix on the getDefaultCode

--- app/model/basicDataTypes/MultiLiteral.java
+++ app/model/basicDataTypes/MultiLiteral.java
@@ -80,7 +80,7 @@ public class MultiLiteral extends HashMap<String, List<String>> {
 			if (!containsKey(defLang)) {
 				int max = 0;
 				for (String k : this.keySet()) {
-					if (!k.equals(Language.DEFAULT.toString())) {
+					if (!k.equals(Language.DEFAULT.getDefaultCode())) {
 						int m = get(k).size();
 						if (max < m) {
 							max = m;
