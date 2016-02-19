72d048d3042b657974e70365cba764a586a8a0d8
gmar gmar <gmar@image.ece.ntua.gr>
2016-02-19 14:07:06 +0200

    small hack for dccreator, dccontributor, dctermsspatial elastic filter

--- app/sources/WithSpaceSource.java
+++ app/sources/WithSpaceSource.java
@@ -135,7 +135,7 @@ public class WithSpaceSource extends ISpaceSource {
 		List<CommonFilter> filters = q.filters;
 		for (CommonFilter f: filters) {
 			for (String filterValue: f.values) {
-                if(f.filterId.equals("dctermsspatial") || f.filterId.equals("dccontributor" || f.filterId.equals("dccreator"))
+                if(f.filterId.equals("dctermsspatial") || f.filterId.equals("dccontributor") || f.filterId.equals("dccreator"))
 	    			elasticoptions.addFilter(f.filterID+".default.all", filterValue);
                 else
 	    			elasticoptions.addFilter(f.filterID+".all", filterValue);
