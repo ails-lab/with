c2ae6e25cf4feb7bc111a8c22cf2134b742aa639
Marios Phinikettos <FinikM@gmail.com>
2016-03-01 20:43:27 +0200

    Setup Gulp to create main.css and added override.less file

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp'),
+    less = require('gulp-less'),
+    rename = require('gulp-rename'),
+    cleanCSS = require('gulp-clean-css');
+
+gulp.task('less', function () {
+  gulp.src('public/css/override.less')
+    .pipe(less())
+    .pipe(cleanCSS())
+    .pipe(rename('main.min.css'))
+    .pipe(gulp.dest('public/css/'));
+});
