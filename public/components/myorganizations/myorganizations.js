be9deb345728132e6f9e71edabf6c06a9081d536
Marios Phinikettos <finikm@gmail.com>
2016-03-04 17:34:04 +0200

    Loading Projects and Organizations

--- public/components/myorganizations/myorganizations.js
+++ public/components/myorganizations/myorganizations.js
@@ -5,10 +5,18 @@ define(['knockout', 'text!./myorganizations.html', 'app'], function (ko, templat
 		create: function (options) {
 			var innerModel = ko.mapping.fromJS(options.data);
 			innerModel.logo = ko.pureComputed(function () {
-				return innerModel.avatar() != null ? innerModel.avatar.Square : 'img/content/profile-placeholder.png';
+				if (typeof innerModel.avatar.Square === 'undefined') {
+					return 'img/content/profile-placeholder.png';
+				} else {
+					return innerModel.avatar.Square();
+				}
 			});
 			innerModel.cover = ko.pureComputed(function () {
-				return innerModel.page.cover() != null ? innerModel.page.cover.Medium : 'img/content/background-space.png';
+				if (typeof innerModel.page.cover.Medium === 'undefined') {
+					return 'img/content/background-space.png';
+				} else {
+					return innerModel.page.cover.Medium();
+				}
 			});
 
 			return innerModel;
@@ -24,6 +32,8 @@ define(['knockout', 'text!./myorganizations.html', 'app'], function (ko, templat
 		var self = this;
 		self.route = params.route;
 		self.groups = ko.observableArray([], mapping);
+		self.name = ko.observable(params.type);
+		self.namePlural = ko.observable(params.title);
 
 		// Check if user is logged in. If not, ask for user to login
 		if (localStorage.getItem('logged_in') != "true") {
@@ -34,9 +44,7 @@ define(['knockout', 'text!./myorganizations.html', 'app'], function (ko, templat
 			url: '/group/list?groupType=' + params.type + '&offset=0&start=0&belongsOnly=true',
 			type: 'GET',
 			success: function (data) {
-				console.log(data);
 				ko.mapping.fromJS(data, mapping, self.groups);
-				console.log(self.groups());
 				WITHApp.tabAction();
 			}
 		});
--- public/js/app/router.js
+++ public/js/app/router.js
@@ -35,7 +35,7 @@ define(["knockout", "crossroads", "hasher"], function (ko, crossroads, hasher) {
 			{ url: 'gallery/{id}/{skin}',     params: { page: '3DRoom',    title: 'Gallery View' } },
 			{ url: 'organization/{id}', params: { page: 'organization-page', title: 'Organization', 'type': 'organization' } },
 			{ url: 'organizations', params: { page: 'organizations', title: 'Organizations', 'type': 'Organization' } },
-			{ url: 'projects', params: { page: 'organizations', title: 'Organizations', 'type': 'Project' } },
+			{ url: 'projects', params: { page: 'organizations', title: 'Projects', 'type': 'Project' } },
 			{ url: 'project/{id}', params: { page: 'organization-page', title: 'Project', 'type': 'project' } },
 			{ url: 'notifications', params: { page: 'notifications-page', title: 'Notifications' } }
 		]
