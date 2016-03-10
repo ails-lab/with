85311f0bfbb57af46270524886255adc0db35bea
Anna Christaki <achristaki@image.ntua.gr>
2016-03-10 12:29:40 +0200

    project name

--- custom/soundspace/js/app/params.js
+++ custom/soundspace/js/app/params.js
@@ -2,7 +2,7 @@ define(function () {
 
     var _args = {}; // private
     
-    _args.projectName = 'Europeana Sounds Space';
+    _args.projectName = 'soundspace';
     _args.projectId = '56e13d2e75fe2450755e553a';
     _args.featuredExhibition='5614f353569e48389d111281';
     _args.featuredCollection='';
