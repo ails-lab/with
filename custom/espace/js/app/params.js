cb8bf0f225eb9cae1162647a5525a2fd038ab0d3
Anna Christaki <achristaki@image.ntua.gr>
2016-04-26 15:39:35 +0300

    different exhibition

--- custom/espace/js/app/params.js
+++ custom/espace/js/app/params.js
@@ -4,7 +4,7 @@ define(function () {
     
     _args.projectName = 'espace';
     _args.projectId = '56cd8aac75fe2461e0871aad';
-    _args.featuredExhibition='56cd993275fe2461e089a8a5';
+    _args.featuredExhibition='56e6725975fe24237ab33d4f';
     _args.featuredCollection='';
     
     return {
