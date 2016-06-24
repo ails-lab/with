b9e5ae8a471fc7f08df3f3c38b986907aa9d0d57
gmar gmar <gmar@image.ece.ntua.gr>
2016-06-24 14:03:35 +0300

    tight search functionality

--- public/custom/soundspace/components/search-page/search.js
+++ public/custom/soundspace/components/search-page/search.js
@@ -175,8 +175,8 @@ define(['bridget', 'knockout', 'text!./search.html', 'isotope', 'imagesloaded', 
 		self.route = params.route;
 		self.term = ko.observable("");
 		self.sourceview=ko.observable(true);
-		self.sources= ko.observableArray([ "Europeana", "WITHin"]);
-		//self.sources= ko.observableArray([ "WITHinASpace"]);
+		//self.sources= ko.observableArray([ "Europeana", "WITHin"]);
+		self.sources= ko.observableArray([ "WITHinASpace"]);
 		self.mixresults=ko.observableArray();
 		self.selectedSource=ko.observable(self.sources()[0]);
 		self.results = ko.observableArray([]);
