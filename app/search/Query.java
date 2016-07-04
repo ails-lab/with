b45589be339c12d6670e09d5a04a222ef355841e
Arne Stabenau <stabenau@gmail.com>
2016-07-04 13:06:20 +0300

    Start of a new search implementation

---
+++ app/search/Query.java
@@ -0,0 +1,32 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+
+package search;
+
+/**
+ * The Query class encapsulates all possible Queries that the system supports within and externally.
+ * 
+ * @author Arne Stabenau
+ *
+ */
+public class Query {
+	public Sources[] sources;
+	
+	/**
+	 * This is the CNF of the query. The inner array filters a meant to be ORed together, the outer  
+	 */
+	public Filter[][] filters;
+}
