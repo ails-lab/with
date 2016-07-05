05c81fcd46b28215dee89d6f5f379d580e457365
Arne Stabenau <stabenau@gmail.com>
2016-07-05 15:40:52 +0300

    More spec for what a source should do

--- app/search/Source.java
+++ app/search/Source.java
@@ -16,20 +16,86 @@
 
 package search;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import play.libs.F.Promise;
+import scalaz.concurrent.PromiseInstances;
+
 /**
- * All potential query sources need an enum entry here. The class that executes the query and 
- * translates Filter fieldnames and values, and prepares WITH model results from the backend results
- * is specified here as Backend. 
+ * Source have the following functionality
+ *  - execute Querys to the backend search API
+ *  - return results in WITH format (translate native response to WITH type)
+ *  - optionally retrieve a complete record based on a search result
+ *  Unclear whether this functionalities have to be exposed
+ *  - translate filter fieldnames to native fieldnames
+ *  - translate filter values to native values
+ *  
+ *  
+ * Problematic is, that some of these functionalities will be executed in varying execution Contexts, sometimes the result
+ * is needed asap and sometimes queuing is requested or required. Lastly, some of the functionalities may need to be executed
+ * on different machines.
+ *  
+ *  
+ * To create a specific Source, extend this source or a suitable other source. Add it to the enumeration Sources.
  * @author Arne Stabenau
  *
  */
-public enum Source {
-	WITHin, Europeana, BritishLibrary
+public class Source {
 	
-	private Backend backendClass;
-	
-	private Source( Backend be ){
-		backendClass = be;
+	/**
+	 * Overwrite this and return the Sources entry that you represent.
+	 * @return
+	 */
+	public Sources thisSource() {
+		return Sources.Europeana;
 	}
 	
+	/**
+	 * Execute this query. Apply some form of load limiting to the backend used. All this requests are
+	 * potential UI requests and should have priority.
+	 * @param query
+	 * @return
+	 */
+	public Promise<Response> execute( Query query ) {
+		return Promise.pure( Response.EMPTY ); 
+	}
+	
+	/**
+	 * Take the incompleteRecord, which is some WITH Record and try to complete it.
+	 * If you need to contact the backend, this is considered a non-priority request.
+	 * @param incompleteRecord
+	 * @return
+	 */
+	public Promise<Object> completeRecord( final Object incompleteRecord ) {
+		return Promise.pure( incompleteRecord );
+	}
+	
+	/**
+	 * Retrieve by Id. Every backend source should be able to get a record by ids that is supplies to its records.
+	 */
+	public Promise<Object> getById( String id ) {
+		return null;
+	}
+	
+	/**
+	 * If you supply this set, you can use the pruneQuery method to have just the filters that
+	 * apply to this source, even to find if the Query actually should be executed on thsi source.
+	 * If no filters remain after pruning, the Query should not be executed.
+	 * @return
+	 */
+	public Set<String> supportedFieldnames() {
+		return new HashSet<String>();
+	}
+	
+	public Query pruneFilters( Query inputQuery ) {
+		return inputQuery.pruneFilters( thisSource(), supportedFieldnames());
+	}
+	
+	/**
+	 * If a source supports autocompleting a query, here it can do it. Return null if you don't autocomplete.
+	 */
+	public Promise<String[]> autocomplete( String partialQueryString ) {
+		return null;
+	}
 }
