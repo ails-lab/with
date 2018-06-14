95f9520ee920a17b8e1df285a3fb65443ce99252
eirini <kaldeli@gmail.com>
2018-06-14 15:47:22 +0300

    Fixed bug with query issued to Europeana with creator value selected returning zero results.

--- app/sources/EuropeanaSpaceSource.java
+++ app/sources/EuropeanaSpaceSource.java
@@ -88,7 +88,7 @@ public class EuropeanaSpaceSource extends ISpaceSource {
 
 		addDefaultQueryModifier(FiltersFields.YEAR.getFilterId(), qDatewriter());
 
-		addDefaultQueryModifier(FiltersFields.CREATOR.getFilterId(), qwriter("CREATOR"));
+		addDefaultQueryModifier(FiltersFields.CREATOR.getFilterId(), qwriter("proxy_dc_creator"));
 
 		// addDefaultWriter(CommonFilters.CONTRIBUTOR_ID,
 		// qfwriter("proxy_dc_contributor"));
@@ -212,9 +212,7 @@ public class EuropeanaSpaceSource extends ISpaceSource {
 	public String getHttpQuery(CommonQuery q) {
 		QueryBuilder builder = new EuropeanaQueryBuilder("http://www.europeana.eu/api/v2/search.json");
 		builder.addSearchParam("wskey", apiKey);
-
 		builder.setQuery("query", q.searchTerm!=null?q.searchTerm:"*");
-
 		if (usingCursor){
 			if (q.page.equals("1"))
 				builder.addSearchParam("cursor", "*");
