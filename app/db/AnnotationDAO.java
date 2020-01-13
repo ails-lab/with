b7b97cb35b5a407f3d3b0a47cff3d07037ce2bbc
Spyros Bekiaris <spirosbekos@gmail.com>
2020-01-13 15:29:28 +0200

    Small adjustment in the db query of the getRecordsByAnnotationLabel call

--- app/db/AnnotationDAO.java
+++ app/db/AnnotationDAO.java
@@ -84,7 +84,7 @@ public class AnnotationDAO extends DAO<Annotation> {
 	public List<Annotation> getByLabel(List<String> generators, String label) {
 		if (generators.size() > 0) {
 			Query<Annotation> q = this.createQuery().disableValidation()
-					.field("body.label.default").equal(label)
+					.field("body.label.en").equal(label)
 					.field("annotators.generator").in(generators)
 					.order("score.approvedBy");
 			List<Annotation> anns = this.find(q).asList();
